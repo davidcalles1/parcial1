df1f1574cc82c066b26e56f97966fe1b051f4b73
davidcalles1 <cc0324032019@unab.edu.sv>
2021-09-09 12:25:42 -0600

    1

---
+++ Parcial1/Parcial1/Controllers/CajeroController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Parcial1.Controllers
+{
+    public class CajeroController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Retiro( int cantidada)
+        {
+            
+            return View();
+        }
+
+        public ActionResult RetiroExitoso()
+        {
+
+            return View();
+        }
+
+        public ActionResult RetiroFallido()
+        {
+
+            return View();
+        }
+    }
+
+    
+    
+    
+
+}
